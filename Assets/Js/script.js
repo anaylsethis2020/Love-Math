f6c23c1cb2e3bb6b8abe074783ab81ef41057323
anaylsethis2020 <70772964+anaylsethis2020@users.noreply.github.com>
2025-05-23 16:06:19 +0100

    Add favicon and final tidy ups for the user

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -15,6 +15,11 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
+    document.getElementById('answer-box').addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            checkAnswer();
+        }
+
     // ADD THIS LINE to start the game with an addition question when the page loads
     runGame("addition");
 
@@ -23,6 +28,9 @@ document.addEventListener("DOMContentLoaded", function () {
      * and after the user's anwser has been processed 
      */
     function runGame(gameType) {
+
+        document.getElementById('answer-box').value = "";
+        document.getElementById('answer-box').focus();
 
         // Creates two random numbers between 1 and 25
         let num1 = Math.floor(Math.random() * 25) + 1;
