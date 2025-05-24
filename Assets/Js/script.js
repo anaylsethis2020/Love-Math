23d5fc8ffd9185b003cf8ce56e706757a8b1ee45
anaylsethis2020 <70772964+anaylsethis2020@users.noreply.github.com>
2025-05-24 15:31:48 +0100

    fix and match files

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -1,11 +1,11 @@
 // Wait for the DOM to finish loading before running the game
 // Get the button elements and add event listeners to them
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
-        button.addEventListener("click", function () {
+        button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
                 checkAnswer();
             } else {
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
-    document.getElementById("answer-box").addEventListener("keydown", function (event) {
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
         if (event.key === "Enter") {
             checkAnswer();
         }
@@ -42,9 +42,9 @@ function runGame(gameType) {
         displayAdditionQuestion(num1, num2);
     } else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
-    } else if (gameType === "subtract") {
+    } else if (gameType === "subtract" ) {
         displaySubtractQuestion(num1, num2);
-    } else if (gameType === "division") {
+    } else if (gameType === "division" ) {
         displayDivisionQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
@@ -91,7 +91,7 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
-    } else if (operator === "÷") {
+    } else if (operator === "/") {
         return [operand1 / operand2, "division"];
     } else {
         alert(`Unimplemented operator ${operator}`);
@@ -117,7 +117,7 @@ function incrementWrongAnswer() {
 
     let oldScore = parseInt(document.getElementById("incorrect").innerText);
     document.getElementById("incorrect").innerText = ++oldScore;
-
+    
 }
 
 function displayAdditionQuestion(operand1, operand2) {
@@ -125,7 +125,7 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "+";
-
+    
 }
 
 function displaySubtractQuestion(operand1, operand2) {
@@ -145,10 +145,8 @@ function displayMultiplyQuestion(operand1, operand2) {
 }
 
 function displayDivisionQuestion(operand1, operand2) {
-    // Ensure division is always exact and no division by zero
-    operand2 = operand2 === 0 ? 1 : operand2;
-    let dividend = operand1 * operand2;
-    document.getElementById('operand1').textContent = dividend;
-    document.getElementById('operand2').textContent = operand2;
-    document.getElementById('operator').textContent = "÷";
+        operand1 = operand1 * operand2;
+        document.getElementById("operand1").textContent = operand1;
+        document.getElementById("operand2").textContent = operand2;
+        document.getElementById("operator").textContent = "/";
 }
